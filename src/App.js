f9fcdb459b8dc9175d775bb67ade50d4c633f529
Nicolas Lerner <lerner.nicolas@gmail.com>
2022-11-12 14:26:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const personas = [
+  {id: 'p01',
+    name: 'Nicolas',
+  },
+  {id: 'p02',
+    name: 'Tincho',
+  },
+  {id: 'p03',
+    name: 'Nandres',
+  }
+]
+const mascotas = [
+  {id: 'm01',
+    name: 'Zafiro',
+  },
+  {id: 'm02',
+    name: 'Mecha',
+  },
+  {id: 'm03',
+    name: 'Clarita',
+  }
+]
+const crosswork = [
+  {id: 'c01',
+    name: 'PushUp',
+  },
+  {id: 'c02',
+    name: 'DeadLift',
+  },
+  {id: 'c03',
+    name: 'BoxJump',
+  }
+]
 
 function App() {
+
+  const [data, setData] = useState(...personas, ...mascotas, ...crosswork)
+  const [current, setCurrent] = useState('all')
+  const [selection, setSelection] = useState(null)
+
+  function handleClick(e) {
+    const location = e.target.filter
+
+    switch (location) {
+      case 'all':
+          setData([...personas, ...mascotas, ...crosswork])  
+          setCurrent('all')
+        break;
+        case 'personas':
+          setData([...personas])  
+          setCurrent('personas')
+        break;
+        case 'mascotas':
+          setData([...mascotas])  
+          setCurrent('mascotas')
+        break;
+        case 'crosswork':
+          setData([...crosswork])  
+          setCurrent('crosswork')
+        break;
+    
+      default:
+        break;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={handleClick} filter='all'>TODOS</button>
+        <button onClick={handleClick} filter='personas'>PERSONAS</button>
+        <button onClick={handleClick} filter='mascotas'>MASCOTAS</button>
+        <button onClick={handleClick} filter='crosswork'>CROSSWORK</button>
+      </div>
+      <div>
+        {data.map((item)=>(
+          <div>{item.name}</div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
